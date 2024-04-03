670dbf82e20f283df407e33f20872f0040451717
Norberto Carosella <norberto.carosella@gmail.com>
2024-04-04 12:17:37 +1300

    remove left and right placeholders

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-row relative">
       <div className="flex bg-blue-400 w-2/3 flex-col items-center relative">
-        left section
         <ImageScrolling/>
       </div>
-      <div className="bg-amber-300 w-1/3 absolute top-0 right-0 z-10 h-[97vh]">
-        right section
-        <div className="h-full overflow-hidden">
+      <div className="w-1/3 absolute top-0 right-0 z-10 h-full">
+        <div className="overflow-hidden">
           <RightScrollArea/>
         </div>
       </div>
