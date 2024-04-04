285f6d415525326708b0efcf8fa4ec6312ce71bc
Norberto Carosella <norberto.carosella@gmail.com>
2024-04-04 20:03:08 +1300

    Finish first part of Welcome page

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -18,8 +18,8 @@ export default function Welcome() {
 				<p className="text-4xl font-normal whitespace-pre-wrap font-serif text-center">Friday <br/> November 22, 2024</p>
 				<p className="text-2xl font-normal whitespace-pre-wrap font-serif mt-8 text-center">Auckland, New Zealand</p>
 			</div>
-			<div className="h-20 flex flex-col items-center" onClick={onButtonClick}>
-				<div>View Details</div>
+			<div className="h-20 flex flex-col items-center cursor-pointer" onClick={onButtonClick}>
+				<div className="mb-6">View Details</div>
 				<div className="h-14">
 					<svg width="66" height="15" viewBox="0 0 66 15" fill="none"
 					     className="">
